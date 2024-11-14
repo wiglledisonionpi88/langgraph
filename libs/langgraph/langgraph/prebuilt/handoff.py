5a0cf910d654ca9045280f2f1126b07064c73023
vbarda <vadym@langchain.dev>
2024-11-14 16:03:42 -0500

    update

--- libs/langgraph/langgraph/prebuilt/handoff.py
+++ libs/langgraph/langgraph/prebuilt/handoff.py
@@ -6,37 +6,36 @@ from langgraph.graph import GraphCommand
 
 
 class HandoffTool(StructuredTool):
-    hand_off_to: str
+    goto: str
 
     @classmethod
-    def from_function(cls, hand_off_to: str, *args, **kwargs):
-        kwargs["hand_off_to"] = hand_off_to
+    def from_function(cls, goto: str, *args, **kwargs):
+        kwargs["goto"] = goto
         return super().from_function(*args, **kwargs)
 
 
 def create_handoff_tool(
-    hand_off_to: str, name: Optional[str] = None, description: Optional[str] = None
+    goto: str, name: Optional[str] = None, description: Optional[str] = None
 ) -> HandoffTool:
     """Create a tool that can hand off control to another node / agent."""
 
     def func():
-        return f"Transferred to '{hand_off_to}'!", GraphCommand(
-            update={"active_agent": hand_off_to}
-        )
+        return f"Transferred to '{goto}'!", GraphCommand(goto=goto)
 
     if description is None:
-        description = f"Transfer to '{hand_off_to}'. Do not ask any details."
+        description = f"Transfer to '{goto}'. Do not ask any details."
 
     if name is None:
-        name = hand_off_to
+        name = goto
 
     transfer_tool = HandoffTool.from_function(
-        hand_off_to,
+        goto,
         func,
         name=name,
         description=description,
         response_format="content_and_artifact",
         args_schema=None,
         infer_schema=True,
+        return_direct=True,
     )
     return transfer_tool
